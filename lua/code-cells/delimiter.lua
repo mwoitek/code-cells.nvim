19f5ac15045e0724e72ff8e1601373a833e9fb01
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-07 08:37:44 -0300

    feat(navigation): find n-th delimiter

--- lua/code-cells/delimiter.lua
+++ lua/code-cells/delimiter.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 -- Find lines that match a pattern {{{
 vim.cmd [[
 function! CellsFindMatchingLines(pattern, first_line, last_line)
@@ -21,5 +23,46 @@ local function find_matching_lines(pattern, first_line, last_line)
   return #line_nums > 0 and line_nums or nil
 end
 -- }}}
+
+-- Find n-th delimiter {{{
+---@param dir cells.Direction
+---@param n integer?
+---@param opts cells.FindOpts?
+---@return integer?
+function M.find_nth(dir, n, opts)
+  n = n or 1
+  opts = opts or {}
+
+  -- TODO: generalize the code for getting the cell delimiter
+  local delim_pattern = "^# %%"
+
+  local first_line ---@type integer
+  local last_line ---@type integer
+
+  local curr_line = vim.fn.line "."
+  if dir == "up" then
+    first_line = 1
+    last_line = opts.include_curr and curr_line or curr_line - 1
+  else
+    first_line = curr_line + 1
+    last_line = vim.api.nvim_buf_line_count(0)
+  end
+
+  local range = require "code-cells.range"
+  if not range.is_valid(first_line, last_line) then return end
+
+  local line_nums = find_matching_lines(delim_pattern, first_line, last_line)
+  if not line_nums then return end
+
+  local line_count = #line_nums
+  if line_count < n then
+    if not opts.allow_less then return end
+    return dir == "up" and line_nums[1] or line_nums[line_count]
+  end
+  return dir == "up" and line_nums[line_count - n + 1] or line_nums[n]
+end
+-- }}}
+
+return M
 
 -- vim: foldmethod=marker:
---
+++ lua/code-cells/types.lua
@@ -0,0 +1,7 @@
+---@meta
+
+---@alias cells.Direction "up"|"down"
+
+---@class cells.FindOpts
+---@field allow_less boolean?
+---@field include_curr boolean?
