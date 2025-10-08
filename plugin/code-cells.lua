17ab084f5edbe52e5f6c6584591d1509db0e2ce5
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-08 02:49:08 -0300

    fix(navigation): remove trailing commas

--- plugin/code-cells.lua
+++ plugin/code-cells.lua
@@ -5,12 +5,12 @@ vim.g.loaded_code_cells = true
 vim.keymap.set(
   "n",
   "<Plug>(CellsJumpPrev)",
-  function() require("code-cells.jump").jump_to_prev(vim.v.count1) end,
+  function() require("code-cells.jump").jump_to_prev(vim.v.count1) end
 )
 vim.keymap.set(
   "n",
   "<Plug>(CellsJumpNext)",
-  function() require("code-cells.jump").jump_to_next(vim.v.count1) end,
+  function() require("code-cells.jump").jump_to_next(vim.v.count1) end
 )
 -- }}}
 
