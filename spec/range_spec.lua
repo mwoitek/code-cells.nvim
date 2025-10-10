fcf65e7b8323476080b23f27c05529961ad608cd
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-10 05:15:20 -0300

    test: add helper functions to handle input files

--- spec/range_spec.lua
+++ spec/range_spec.lua
@@ -1,5 +1,19 @@
+---@param file_name string
+local function edit_file(file_name)
+  local file_path = vim.fs.joinpath("spec", "fixtures", file_name)
+  vim.cmd.edit(file_path)
+end
+
+local function unload_buffer()
+  local file_path = vim.api.nvim_buf_get_name(0)
+  if file_path == "" then return end
+  vim.cmd "bd!"
+end
+
 describe("code-cells.range", function()
   local range = require "code-cells.range"
+
+  after_each(unload_buffer)
 
   describe(".is_valid()", function()
     local is_valid = range.is_valid
@@ -11,9 +25,7 @@ describe("code-cells.range", function()
     end)
 
     it("returns false if the last line is greater than the line count", function()
-      local file_path = "spec/fixtures/01.py"
-      vim.cmd.edit { args = { file_path }, bang = true }
-
+      edit_file "01.py"
       local first_line = 1
       local last_line = 9999
       assert.is_false(is_valid(first_line, last_line))
