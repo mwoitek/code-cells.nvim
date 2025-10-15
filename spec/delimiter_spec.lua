f809840f484f6ddffe8ef274e57d1eb36fccdb60
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-15 20:06:06 -0300

    test(delimiter): add tests for find_above

--- spec/delimiter_spec.lua
+++ spec/delimiter_spec.lua
@@ -64,6 +64,70 @@ describe("code-cells.api.delimiter", function()
     end)
   end)
 
+  describe(".find_above()", function()
+    it("finds every match above the current line", function()
+      helpers.edit_file "05.js"
+      helpers.source_ftplugin()
+      local init_pos = { 15, 4 }
+      vim.api.nvim_win_set_cursor(0, init_pos)
+      local matches = delimiter.find_above()
+      local exp_matches = { 4, 1 }
+      assert.are.same(matches, exp_matches)
+    end)
+
+    it("finds every match above the current line when the delimiter is explicitly given", function()
+      helpers.edit_file "02.R"
+      local init_pos = { 24, 4 }
+      vim.api.nvim_win_set_cursor(0, init_pos)
+      local delim = "# %%"
+      local matches = delimiter.find_above(delim)
+      local exp_matches = { 23, 4, 1 }
+      assert.are.same(matches, exp_matches)
+    end)
+
+    it("finds every match above a specified line (exclusive)", function()
+      helpers.edit_file "09.lua"
+      helpers.source_ftplugin()
+      local matches = delimiter.find_above(nil, {
+        line = 13,
+        include_line = false,
+      })
+      local exp_matches = { 4, 1 }
+      assert.are.same(matches, exp_matches)
+    end)
+
+    it("finds every match above a specified line (inclusive)", function()
+      helpers.edit_file "09.lua"
+      helpers.source_ftplugin()
+      local matches = delimiter.find_above(nil, {
+        line = 13,
+        include_line = true,
+      })
+      local exp_matches = { 13, 4, 1 }
+      assert.are.same(matches, exp_matches)
+    end)
+
+    it("finds every match above a specified line while respecting `max_matches`", function()
+      helpers.edit_file "09.lua"
+      helpers.source_ftplugin()
+      local matches = delimiter.find_above(nil, {
+        line = 25,
+        max_matches = 3,
+      })
+      local exp_matches = { 21, 18, 13 }
+      assert.are.same(matches, exp_matches)
+    end)
+
+    it("returns nil when no match is found above", function()
+      helpers.edit_file "06.clj"
+      helpers.source_ftplugin()
+      local init_pos = { 1, 14 }
+      vim.api.nvim_win_set_cursor(0, init_pos)
+      local matches = delimiter.find_above()
+      assert.is_nil(matches)
+    end)
+  end)
+
   describe(".find_nth()", function()
     it("TODO", function()
       -- TODO
