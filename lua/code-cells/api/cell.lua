78961720b94fab9c099d91aa312b7c89a55fcfbc
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-17 17:03:01 -0300

    refactor(cell): rewrite Cell class

--- lua/code-cells/api/cell.lua
+++ lua/code-cells/api/cell.lua
@@ -7,7 +7,7 @@ local fn = vim.fn
 ---@return boolean # true if input is empty, false otherwise
 local function is_empty_string(s)
   local start = string.find(s, "^%s*$")
-  return type(start) == "number"
+  return start ~= nil
 end
 
 ---@class cells.Cell Code cell
@@ -16,12 +16,12 @@ end
 local Cell = {}
 Cell.__index = Cell
 
----@param first_line integer? Cell's first line
+---@param first_line integer Cell's first line
 ---@param last_line integer? Cell's last line
 ---@return cells.Cell # Code cell
 function Cell.new(first_line, last_line)
   local obj = {
-    first_line = first_line or 1,
+    first_line = first_line,
     last_line = last_line or api.nvim_buf_line_count(0),
   }
   return setmetatable(obj, Cell)
@@ -32,55 +32,60 @@ end
 ---@return boolean # true if cells are equal, false otherwise
 function Cell.__eq(c1, c2) return c1.first_line == c2.first_line and c1.last_line == c2.last_line end
 
----@return cells.Cell # Trimmed code cell
-function Cell:trim_top()
-  local new_first = self.first_line + 1
-  local lstr ---@type string?
+---@return integer # First line of the cell's inner part
+---@return integer # Last line of the cell's inner part
+function Cell:inner()
+  local first = self.first_line + 1
+  local last = self.last_line
+  if first > last then return self.first_line, self.last_line end
 
-  while new_first <= self.last_line do
-    lstr = fn.getline(new_first)
+  local lstr = fn.getline(last)
+  if not is_empty_string(lstr) then return first, last end
+
+  last = last - 1
+  if last >= first then return first, last end
+  return self.first_line, self.last_line
+end
+
+---@return integer? # Cell's first non-blank line
+function Cell:first_non_blank()
+  local first = self.first_line + 1
+
+  while first <= self.last_line do
+    local lstr = fn.getline(first)
     if is_empty_string(lstr) then
-      new_first = new_first + 1
+      first = first + 1
     else
       break
     end
   end
 
-  if new_first > self.last_line then
-    return lstr and Cell.new(self.first_line + 1, self.last_line) or self
-  end
-  return Cell.new(new_first, self.last_line)
+  if first <= self.last_line then return first end
 end
 
----@return cells.Cell # Trimmed code cell
-function Cell:trim_bottom()
-  local new_last = self.last_line
-  local lstr ---@type string?
+---@return integer? # Cell's last non-blank line
+function Cell:last_non_blank()
+  local last = self.last_line
 
-  while new_last > self.first_line do
-    lstr = fn.getline(new_last)
+  while last > self.first_line do
+    local lstr = fn.getline(last)
     if is_empty_string(lstr) then
-      new_last = new_last - 1
+      last = last - 1
     else
       break
     end
   end
 
-  if new_last == self.first_line then
-    return lstr and Cell.new(self.first_line, self.first_line + 1) or self
-  end
-  return Cell.new(self.first_line, new_last)
+  if last > self.first_line then return last end
 end
 
----@param skip_lead_blanks boolean? Skip leading blank lines?
----@return cells.Cell # Inner part of the given cell
-function Cell:inner(skip_lead_blanks)
-  if skip_lead_blanks == nil then skip_lead_blanks = true end
-  local inner = self:trim_bottom()
-  if skip_lead_blanks then return inner:trim_top() end
-  local new_first = inner.first_line + 1
-  if new_first <= inner.last_line then inner.first_line = new_first end
-  return inner
+---@return integer # First line of the cell's core part
+---@return integer # Last line of the cell's core part
+function Cell:core()
+  local first = self:first_non_blank()
+  if not first then return self:inner() end
+  local last = assert(self:last_non_blank())
+  return first, last
 end
 
 M.Cell = Cell
