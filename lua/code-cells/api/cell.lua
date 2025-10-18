4ffa5c899b7a6dcfc7ca9872fee49a0956311086
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-18 15:24:47 -0300

    feat(cell): add `jump` method to Cell

--- lua/code-cells/api/cell.lua
+++ lua/code-cells/api/cell.lua
@@ -27,8 +27,8 @@ function Cell.new(first_line, last_line)
   return setmetatable(obj, Cell)
 end
 
----@return integer # First line of the cell's inner part
----@return integer # Last line of the cell's inner part
+---@return integer # First line of the cell's inner layer
+---@return integer # Last line of the cell's inner layer
 function Cell:inner()
   local first = self.first_line + 1
   local last = self.last_line
@@ -74,13 +74,38 @@ function Cell:last_non_blank()
   if last > self.first_line then return last end
 end
 
----@return integer # First line of the cell's core part
----@return integer # Last line of the cell's core part
+---@return integer # First line of the cell's core layer
+---@return integer # Last line of the cell's core layer
 function Cell:core()
   local first = self:first_non_blank()
   if not first then return self:inner() end
   local last = assert(self:last_non_blank())
   return first, last
+end
+
+---@alias cells.cell.Layer "outer"|"inner"|"core"
+
+---@param layer cells.cell.Layer Cell layer
+---@return integer # Cell's first line for the given layer
+---@return integer # Cell's last line for the given layer
+function Cell:range(layer)
+  if layer == "outer" then
+    return self.first_line, self.last_line
+  elseif layer == "inner" then
+    return self:inner()
+  else
+    return self:core()
+  end
+end
+
+---@alias cells.cell.Position "first"|"last"
+
+---@param layer cells.cell.Layer Cell layer
+---@param position cells.cell.Position Jump position (first or last line)
+function Cell:jump(layer, position)
+  local first, last = self:range(layer)
+  local line = position == "first" and first or last
+  api.nvim_win_set_cursor(0, { line, 0 })
 end
 
 M.Cell = Cell
