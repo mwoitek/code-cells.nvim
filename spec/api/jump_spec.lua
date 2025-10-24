5966603381800400b7118151d9a35e47ef3ead32
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-24 16:10:00 -0300

    test(jump): improve tests for `to_next`

--- spec/api/jump_spec.lua
+++ spec/api/jump_spec.lua
@@ -1,6 +1,8 @@
 local assert = require("luassert")
 local utils = require("spec.utils")
 
+local api = vim.api
+
 describe("code-cells.api.jump", function()
   local jump = require("code-cells.api.jump")
 
@@ -9,7 +11,7 @@ describe("code-cells.api.jump", function()
   after_each(utils.unload_buffer)
 
   describe(".to_next()", function()
-    it("throws an error when the type of `count` is wrong", function()
+    it("throws an error when `count` is invalid", function()
       local delimiter = "-- %%"
       local invalid_input = {
         boolean = true,
@@ -23,81 +25,101 @@ describe("code-cells.api.jump", function()
       end
     end)
 
-    it("moves the cursor as expected when default arguments are used", function()
+    it("jumps as expected when default arguments are used", function()
       utils.load_fixture("01.py")
+
       local init_pos = { 11, 28 }
-      vim.api.nvim_win_set_cursor(0, init_pos)
+      api.nvim_win_set_cursor(0, init_pos)
+
       jump.to_next()
-      local new_pos = vim.api.nvim_win_get_cursor(0)
+
+      local new_pos = api.nvim_win_get_cursor(0)
       local exp_pos = { 15, 0 }
       assert.are.same(new_pos, exp_pos)
     end)
 
-    it("moves the cursor as expected when the delimiter is explicitly given", function()
+    it("jumps as expected when the delimiter is given explicitly", function()
       utils.load_fixture("02.R")
+
       local init_pos = { 12, 6 }
-      vim.api.nvim_win_set_cursor(0, init_pos)
+      api.nvim_win_set_cursor(0, init_pos)
+
       local delimiter = "# %%"
-      local count = 1
-      jump.to_next(delimiter, count)
-      local new_pos = vim.api.nvim_win_get_cursor(0)
+      jump.to_next(delimiter)
+
+      local new_pos = api.nvim_win_get_cursor(0)
       local exp_pos = { 23, 0 }
       assert.are.same(new_pos, exp_pos)
     end)
 
-    it("moves the cursor as expected when the count is greater than 1", function()
+    it("jumps as expected when the count is greater than 1", function()
       utils.load_fixture("03.jl")
+
       local init_pos = { 2, 17 }
-      vim.api.nvim_win_set_cursor(0, init_pos)
+      api.nvim_win_set_cursor(0, init_pos)
+
       local count = 2
       jump.to_next(nil, count)
-      local new_pos = vim.api.nvim_win_get_cursor(0)
+
+      local new_pos = api.nvim_win_get_cursor(0)
       local exp_pos = { 15, 0 }
       assert.are.same(new_pos, exp_pos)
     end)
 
     it(
-      "moves the cursor as expected when the count is greater than the number of cells that follow",
+      "jumps as expected when the count is greater than the number of cells that follow",
       function()
         utils.load_fixture("05.js")
+
         local init_pos = { 3, 0 }
-        vim.api.nvim_win_set_cursor(0, init_pos)
+        api.nvim_win_set_cursor(0, init_pos)
+
         local count = 10
         jump.to_next(nil, count)
-        local new_pos = vim.api.nvim_win_get_cursor(0)
+
+        local new_pos = api.nvim_win_get_cursor(0)
         local exp_pos = { 19, 0 }
         assert.are.same(new_pos, exp_pos)
       end
     )
 
-    it("moves the cursor as expected when the count is negative", function()
+    it("jumps as expected when the count is negative", function()
       utils.load_fixture("06.clj")
+
       local init_pos = { 6, 25 }
-      vim.api.nvim_win_set_cursor(0, init_pos)
+      api.nvim_win_set_cursor(0, init_pos)
+
       local count = -1
       jump.to_next(nil, count)
-      local new_pos = vim.api.nvim_win_get_cursor(0)
+
+      local new_pos = api.nvim_win_get_cursor(0)
       local exp_pos = { 4, 0 }
       assert.are.same(new_pos, exp_pos)
     end)
 
     it("does nothing when there is no next cell", function()
       utils.load_fixture("04.sql")
+
       local init_pos = { 10, 11 }
-      vim.api.nvim_win_set_cursor(0, init_pos)
-      local count = 1
-      jump.to_next(nil, count)
-      local new_pos = vim.api.nvim_win_get_cursor(0)
+      api.nvim_win_set_cursor(0, init_pos)
+
+      jump.to_next()
+
+      local new_pos = api.nvim_win_get_cursor(0)
       assert.are.same(new_pos, init_pos)
     end)
 
-    it("does nothing when the cursor is initally at the bottom of the buffer", function()
+    it("does nothing when the cursor is initially at the bottom of the buffer", function()
       utils.load_fixture("04.sql")
-      local init_pos = { vim.fn.line("$"), 0 }
-      vim.api.nvim_win_set_cursor(0, init_pos)
-      local count = 1
-      jump.to_next(nil, count)
-      local new_pos = vim.api.nvim_win_get_cursor(0)
+
+      local last_line = api.nvim_buf_line_count(0)
+      local last_col = vim.fn.charcol({ last_line, "$" }) - 2
+      local init_pos = { last_line, last_col }
+      api.nvim_win_set_cursor(0, init_pos)
+
+      jump.to_next()
+
+      local new_pos = api.nvim_win_get_cursor(0)
       assert.are.same(new_pos, init_pos)
     end)
   end)
