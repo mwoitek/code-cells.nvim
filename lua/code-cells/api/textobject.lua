662d7c1d7d40e5a36da272249b5c288603b13dca
Marcio Woitek <mwoitekjr@gmail.com>
2025-10-25 21:23:08 -0300

    refactor(textobject): add a few helper functions

--- lua/code-cells/api/textobject.lua
+++ lua/code-cells/api/textobject.lua
@@ -1,5 +1,40 @@
 local M = {}
 
+local api = vim.api
+local fn = vim.fn
+
+---@param modes string|string[]
+local function assert_mode(modes)
+  local mode = api.nvim_get_mode().mode
+  local ok ---@type boolean
+  if type(modes) == "string" then
+    ok = modes == mode
+  else
+    ok = vim.tbl_contains(modes, mode)
+  end
+  assert(ok, "invalid mode")
+end
+
+---@return integer
+---@return integer
+local function get_line_range_from_selection()
+  assert_mode("V")
+  vim.cmd("normal! V")
+  local _, first = unpack(fn.getcharpos("'<"))
+  local _, last = unpack(fn.getcharpos("'>"))
+  return first, last
+end
+
+---@param first integer
+---@param last integer
+local function select_line_range(first, last)
+  assert_mode("n")
+  api.nvim_win_set_cursor(0, { first, 0 })
+  vim.cmd("normal! V")
+  local last_col = fn.charcol({ last, "$" }) - 2
+  api.nvim_win_set_cursor(0, { last, last_col })
+end
+
 ---@param delimiter string? Cell delimiter
 ---@param layer cells.CellLayer Cell layer
 function M.textobject(delimiter, layer)
